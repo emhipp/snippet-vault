7d04e7943e00dfeea60b97d78ab993ed7bcc8595
emhipp <ehipp@clairity.com>
2025-09-24 13:48:37 -0600

    aws options and read/write to s3

--- R/aws.R
+++ R/aws.R
@@ -1,11 +1,27 @@
 # reading CSV file from s3 bucket w/ date formatting
-read_csv_from_s3 <- function(bucket, key) {
+read_csv_from_s3 <- function(bucket, key, version=NULL, header=TRUE) {
   out <-  data.table::fread(
     rawToChar(
       paws::s3()$get_object(
         Bucket = bucket,
-        Key = key
+        Key = key,
+        VersionId = version
       )$Body)) |>
-    dplyr::mutate(across(lubridate::is.instant, as.Date))
+    dplyr::mutate(across(lubridate::is.instant, as.Date), header=header)
   return(out)
 }
+
+# write data frame as CSV file to s3
+write_csv_to_s3 <- function(df, bucket, key, col_names=TRUE) {
+  withr::with_tempfile("temp", {
+    readr::write_csv(df, temp, col_names=col_names)
+    paws::s3()$put_object(
+      Body = temp,
+      Bucket = bucket,
+      Key = key
+    )
+  })
+}
+
+# set AWS SSO parameters
+Sys.setenv(AWS_PROFILE = "")
--- R/options.R
+++ R/options.R
@@ -2,3 +2,6 @@
 options(repr.matrix.max.cols=300, repr.matrix.max.rows=200)
 options(dplyr.width = Inf)
 options(max.print = 10000)
+
+# set AWS SSO parameters
+Sys.setenv(AWS_PROFILE = "")
